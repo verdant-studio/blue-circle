d33cca166c4b817b44d75939f52f32d321f73fc6
Robert Bossaert <contact@robertbossaert.com>
2021-12-23 07:30:11 +0100

    (feat): add users table to front-end

---
+++ resources/lang/nl/users.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Users Language Lines
+    |--------------------------------------------------------------------------
+    */
+
+    '_' => 'Gebruikers',
+    'actions' => 'Acties',
+    'created-at' => 'Aangemaakt op',
+    'edit' => 'Bewerken',
+    'delete' => 'Verwijderen',
+    'name' => 'Naam',
+    'role' => 'Rol',
+];
--- resources/views/admin/user/index.blade.php
+++ resources/views/admin/user/index.blade.php
@@ -1,5 +1,59 @@
-Hello World
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="text-xl font-semibold leading-tight text-gray-800">
+            {{ __('users._') }}
+        </h2>
+    </x-slot>
 
-@foreach ($users as $user)
-    {{ $user->name }}
-@endforeach
+    <div class="py-12">
+        <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+
+            <div class="mb-8 overflow-hidden border-b border-gray-200 rounded-md shadow">
+                <table class="min-w-full divide-y divide-gray-200">
+                    <thead class="bg-gray-50">
+                        <tr>
+                            <th class="px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase" scope="col">{{ __('users.name') }}</th>
+                            <th class="px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase" scope="col">{{ __('users.role') }}</th>
+                            <th class="px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase" scope="col">{{ __('users.created-at') }}</th>
+                            <th class="px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase" scope="col">{{ __('users.actions') }}</th>
+                        </tr>
+                    </thead>
+
+                    <tbody class="bg-white divide-y divide-gray-200">
+                    @foreach ($users as $user)
+                        <tr>
+                        <td class="px-6 py-4 whitespace-nowrap">
+                            <span class="text-gray-500">{{ $user->name }}</span>
+                        </td>
+                        <td class="px-6 py-4 whitespace-nowrap">
+                            @foreach ($user->roles as $role)
+                                <span class="text-gray-500">{{ $role->name }}</span>
+                            @endforeach
+                        </td>
+                        <td class="px-6 py-4 whitespace-nowrap">
+                            {{ $user->created_at->format('j F, Y') }}
+                        </td>
+                        <td class="px-6 py-4 whitespace-nowrap">
+                            <a class="inline-block hover:text-primary-900 text-primary-500" href="">
+                                <svg class="w-6 h-6 mr-2" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
+                                </svg>
+                                <span class="sr-only">{{ __('users.edit') }}</span>
+                            </a>
+                            <button class="inline-block text-red-600 hover:text-red-900" type="submit">
+                                <svg class="w-6 h-6 mr-2" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+                                </svg>
+                                <span class="sr-only">{{ __('users.delete') }}</span>
+                            </button>
+                            </div>
+                        </td>
+                        </tr>
+                    @endforeach
+                    </tbody>
+                </table>
+            </div>
+
+        </div>
+    </div>
+</x-app-layout>
