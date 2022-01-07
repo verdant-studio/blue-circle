b6661490466fc1d60996ecc1469f14f35ac606fa
Robert Bossaert <contact@robertbossaert.com>
2022-01-07 11:14:38 +0100

    (feat): change logo

--- resources/views/policy.blade.php
+++ resources/views/policy.blade.php
@@ -2,7 +2,7 @@
     <div class="pt-4 bg-slate-100">
         <div class="flex flex-col items-center min-h-screen pt-6 sm:pt-0">
             <div>
-                <x-jet-authentication-card-logo />
+                <x-jet-application-logo />
             </div>
 
             <div class="w-full p-6 mt-6 overflow-hidden prose bg-white shadow-md sm:max-w-2xl sm:rounded-lg">
--- resources/views/terms.blade.php
+++ resources/views/terms.blade.php
@@ -2,7 +2,7 @@
     <div class="pt-4 bg-slate-100">
         <div class="min-h-screen flex flex-col items-center pt-6 sm:pt-0">
             <div>
-                <x-jet-authentication-card-logo />
+                <x-jet-application-logo />
             </div>
 
             <div class="w-full sm:max-w-2xl mt-6 p-6 bg-white shadow-md overflow-hidden sm:rounded-lg prose">
