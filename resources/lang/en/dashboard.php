f802d2fa720891c9a74feca025560a9194411787
Robert Bossaert <contact@robertbossaert.com>
2022-01-11 19:41:19 +0100

    (feat): display GA data on dashboard (#12)

--- resources/lang/en/dashboard.php
+++ resources/lang/en/dashboard.php
@@ -13,4 +13,9 @@ return [
     'welcome-description' => 'Blue Circle is a web application that allows you (and your visitors) to build a directory of links. The code is open source and can be found at',
     'welcome-text' => 'Blue Circle',
     'welcome-user' => 'Welcome :name!',
+    'stats' => [
+        'most-visited-pages' => 'Most visited pages in the last :days days',
+        'referrers' => 'Top referrers in the last :days days',
+        'total-visits' => 'Total visits in the the past year',
+    ]
 ];
