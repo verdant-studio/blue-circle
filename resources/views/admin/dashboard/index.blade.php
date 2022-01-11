4fe1df62a0e1ebc6c2c5ba43629b54f521bb859e
Robert Bossaert <contact@robertbossaert.com>
2022-01-11 08:36:30 +0100

    (feat): stats

--- resources/views/admin/dashboard/index.blade.php
+++ resources/views/admin/dashboard/index.blade.php
@@ -6,10 +6,20 @@
     </x-slot>
 
     <div class="py-12">
-        <div class="mx-auto max-w-7xl px-4">
+        <div class="px-4 mx-auto max-w-7xl">
             <div class="overflow-hidden bg-white shadow-xl sm:rounded-lg">
                 <x-jet-welcome />
             </div>
         </div>
+    </div>
+
+    <div class="tab-pane statistic-tabs active" id="pages">
+        <ul class="item-list">
+            @foreach($statistics['pages'] as $p)
+            <li>
+                {{ $p['url'] }}<span class="pull-right"> {{ $p['pageViews'] }}</span>
+            </li>
+            @endforeach
+        </ul>
     </div>
 </x-app-layout>
