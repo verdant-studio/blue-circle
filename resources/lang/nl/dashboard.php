9a50ec36ccec541666224473472b309e94ccf20b
Robert Bossaert <contact@robertbossaert.com>
2022-01-11 19:36:21 +0100

    (feat): add stats

--- resources/lang/nl/dashboard.php
+++ resources/lang/nl/dashboard.php
@@ -13,4 +13,9 @@ return [
     'welcome-description' => 'Blue Circle is een webapplicatie waarmee jij en/of je bezoekers eigen startpagina\'s kunnen beheren. De code is open source en is te vinden op',
     'welcome-text' => 'Blue Circle',
     'welcome-user' => 'Welkom :name!',
+    'stats' => [
+        'most-visited-pages' => 'Meest bezochte pagina\'s in de afgelopen :days dagen',
+        'referrers' => 'Top verwijzers in de afgelopen :days dagen',
+        'total-visits' => 'Totaal aantal bezoeken in het afgelopen jaar',
+    ]
 ];
--- resources/views/admin/dashboard/index.blade.php
+++ resources/views/admin/dashboard/index.blade.php
@@ -1,19 +1,23 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h1 class="text-xl font-semibold leading-tight text-white">
-            {{ __('dashboard._') }}
-        </h1>
-    </x-slot>
+    <div class="mb-6 md:mb-12">
+        <x-slot name="header">
+            <h1 class="text-xl font-semibold leading-tight text-white">
+                {{ __('dashboard._') }}
+            </h1>
+        </x-slot>
+    </div>
 
-    <div class="py-12">
+    <div class="mb-4 md:mb-8">
         <div class="px-4 mx-auto max-w-7xl">
-            <div class="overflow-hidden bg-white shadow-xl sm:rounded-lg">
+            <div class="overflow-hidden bg-white shadow-md sm:rounded-lg">
                 <x-jet-welcome />
             </div>
         </div>
     </div>
 
     @can('stats read')
+    <div class="px-4 mx-auto max-w-7xl">
         @livewire('admin.components.stats')
+    </div>
     @endcan
 </x-app-layout>
