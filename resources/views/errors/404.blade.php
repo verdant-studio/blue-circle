4c431a12078af983b2511d45f9ff37999de73b2f
Robert Bossaert <contact@robertbossaert.com>
2022-01-17 19:40:29 +0100

    (feat): style error pages

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.error', [
+    'title' => __('general.error.page-404-title'),
+    'description' => __('general.error.page-404-description'),
+    'homeLink' => true
+])
---
+++ resources/views/errors/500.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.error', [
+    'title' => __('general.error.page-500-title'),
+    'description' => __('general.error.page-500-description'),
+    'homeLink' => false
+])
