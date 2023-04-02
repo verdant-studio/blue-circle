d19d3917f43e07554087d20a80d0f520b2ea6e09
Robert Bossaert <contact@robertbossaert.com>
2023-04-02 09:54:16 +0200

    (feat): add deploy.php

---
+++ deploy.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Deployer;
+
+if (file_exists('.env')) {
+    $dotenv = \Dotenv\Dotenv::createImmutable(__DIR__);
+    $dotenv->load();
+}
+
+require 'recipe/common.php';
+
+set('application', $_ENV['APP_NAME']);
+set('timezone', 'Europe/Amsterdam');
+set('repository', 'git@github.com:verdant-studio/blue-circle.git');
+set('keep_releases', 3);
+set('git_tty', true);
+set('allow_anonymous_stats', false);
+set('default_stage', 'production');
+set('shared_files', [
+    '.env',
+    'public/sitemap.xml'
+]);
+set('shared_dirs', [
+    'storage/app/analytics',
+    'storage/app/public',
+    'storage/framework/cache',
+    'storage/framework/sessions',
+    'storage/framework/logs',
+    'storage/framework/views',
+]);
+
+// Hosts
+host($_ENV['DEPLOYER_PROD_HOST'])
+    ->stage('production')
+    ->user($_ENV['DEPLOYER_PROD_USER'])
+    ->port($_ENV['DEPLOYER_PROD_PORT'])
+    ->set('deploy_path', '')
+    ->set('branch', 'master')
+    ->set('http_user', $_ENV['DEPLOYER_PROD_USER'])
+    ->set('domain', '')
+    ->set('composer_options', 'install --verbose --prefer-dist --no-progress --no-interaction --optimize-autoloader --ignore-platform-reqs')
+    ->set('writable_mode', 'chmod')
+    ->set('writable_use_sudo', true)
+    ->set('ssh_multiplexing', true);
+
+// Tasks
+desc('Deploy your project');
+task('deploy', [
+    'deploy:info',
+    'deploy:prepare',
+    'deploy:lock',
+    'deploy:release',
+    'deploy:update_code',
+    'deploy:shared',
+    'deploy:vendors',
+    'deploy:assets',
+    'deploy:clear_paths',
+    'deploy:symlink',
+    'deploy:unlock',
+    'cleanup',
+    'success',
+]);
+
+// [Optional] If deploy fails automatically unlock.
+after('deploy:failed', 'deploy:unlock');
+
+/**
+ * Compile assets on production
+ */
+task('deploy:assets', function () {
+    runLocally('npm install && npm run prod');
+    upload('./public', '{{release_path}}/.');
+})->desc('Generating assets and uploading them to server');
